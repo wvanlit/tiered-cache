a818212b029104574190802d5ed7607dd31189d1
Wessel van Lit <wessel@litdev.nl>
2025-07-05 13:15:06 +0200

    Simple impl - just set/get with soft expiry

---
+++ src/CacheLayer.ts
@@ -0,0 +1,149 @@
+import Keyv from "keyv";
+
+// Only JSON-compatible values are cacheable
+type CacheablePrimitive = string | number | boolean | null | undefined;
+type Cacheable = CacheablePrimitive | Cacheable[] | { [key: string]: Cacheable };
+
+export interface CacheItem<T extends Cacheable> {
+  key: string;
+  value: T;
+  memoryOnly?: boolean;
+}
+
+export interface CacheLayer {
+  get<T extends Cacheable>(key: string, allowExpired: boolean): Promise<T | null>;
+  set<T extends Cacheable>(item: CacheItem<T>): Promise<void>;
+
+  getMany<T extends Cacheable>(...keys: string[]): Promise<(T | null)[]>;
+  setMany<T extends Cacheable>(...items: CacheItem<T>[]): Promise<void>;
+
+  getOrSet<T extends Cacheable>(key: string, factory: () => Promise<T>): Promise<T>;
+  getOrSetMany<T extends Cacheable>(keys: string[], factory: (missing: string[]) => Promise<T[]>): Promise<T[]>;
+}
+
+export type CacheSettings = {
+  ttl: {
+    memory: {
+      /**
+       * Seconds until data is considered stale
+       */
+      soft: number;
+
+      /**
+       * Seconds until data expires
+       */
+      strict: number;
+    };
+    distributed: {
+      /**
+       * Seconds until data is considered stale
+       */
+      soft: number;
+
+      /**
+       * Seconds until data expires
+       */
+      strict: number;
+    };
+  };
+
+  timeouts: {
+    /**
+     * Returns stale data if available - in millis
+     */
+    soft: number;
+
+    /**
+     * Throws an exception when passed - in millis
+     */
+    strict: number;
+  };
+
+  allowBackgroundUpdates?: boolean;
+};
+
+export type InternalCacheItem<T> = {
+  value: T;
+  // For soft ttl - Unix Timestamp
+  expiredAfter: number;
+};
+
+const MS_IN_A_SEC = 1000;
+
+export class TieredCacheLayer implements CacheLayer {
+  private readonly settings: CacheSettings;
+  private readonly memory: Keyv;
+  private readonly distributed: Keyv;
+
+  constructor(settings: CacheSettings, memory: Keyv, distributed: Keyv) {
+    this.settings = settings;
+    this.memory = memory;
+    this.distributed = distributed;
+  }
+
+  async get<T extends Cacheable>(key: string, allowExpired: boolean): Promise<T | null> {
+    const [memoryItem, memoryExpired] = await this.getFromCache<T>(this.memory, key);
+    if (memoryItem && !memoryExpired) {
+      return memoryItem.value;
+    }
+
+    const [distributedItem, distributedExpired] = await this.getFromCache<T>(this.distributed, key);
+    if (distributedItem && (!distributedExpired || allowExpired)) {
+      return distributedItem.value;
+    }
+
+    if (allowExpired && distributedExpired && memoryItem) {
+      return memoryItem!.value;
+    }
+
+    return null;
+  }
+
+  async set<T extends Cacheable>(item: CacheItem<T>): Promise<void> {
+    if (item.memoryOnly) {
+      await this.setInCache(this.memory, item);
+      return;
+    }
+
+    await Promise.allSettled([this.setInCache(this.memory, item), this.setInCache(this.distributed, item)]);
+  }
+
+  getMany<T extends Cacheable>(...keys: string[]): Promise<(T | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  setMany<T extends Cacheable>(...items: CacheItem<T>[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  getOrSet<T extends Cacheable>(key: string, factory: () => Promise<T>): Promise<T> {
+    throw new Error("Method not implemented.");
+  }
+  getOrSetMany<T extends Cacheable>(keys: string[], factory: (missing: string[]) => Promise<T[]>): Promise<T[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  private setInCache<T extends Cacheable>(cache: Keyv, item: CacheItem<T>) {
+    const isMemory = cache === this.memory;
+    const setting = isMemory ? this.settings.ttl.memory : this.settings.ttl.distributed;
+
+    return cache.set<InternalCacheItem<T>>(
+      item.key,
+      { value: item.value, expiredAfter: this.now() + setting.soft * MS_IN_A_SEC },
+      setting.strict * MS_IN_A_SEC,
+    );
+  }
+
+  private now() {
+    return Date.now();
+  }
+
+  private async getFromCache<T extends Cacheable>(cache: Keyv, key: string) {
+    const memoryItem = await cache.get<InternalCacheItem<T>>(key);
+
+    let expired = true;
+    if (memoryItem) {
+      expired = memoryItem.expiredAfter < this.now();
+    }
+
+    return [memoryItem, expired] as const;
+  }
+}
---
+++ test/unit/read-write.test.ts
@@ -0,0 +1,117 @@
+import Keyv from "keyv";
+import { describe, it, expect, vitest, beforeEach } from "vitest";
+import { CacheLayer, TieredCacheLayer } from "../../src/CacheLayer";
+
+vitest.useFakeTimers();
+
+let sut: CacheLayer;
+let memoryCache: Keyv;
+let distributedCache: Keyv;
+
+const memorySoftTtl = 1;
+const memoryStrictTtl = 2;
+const distributedSoftTtl = 5;
+const distributedStrictTtl = 10;
+
+beforeEach(() => {
+  memoryCache = new Keyv();
+  // We're "mocking" the distributed cache using a memory cache
+  distributedCache = new Keyv();
+
+  sut = new TieredCacheLayer(
+    {
+      timeouts: {
+        soft: 100,
+        strict: 200,
+      },
+      ttl: {
+        memory: {
+          soft: memorySoftTtl,
+          strict: memoryStrictTtl,
+        },
+        distributed: {
+          soft: distributedSoftTtl,
+          strict: distributedStrictTtl,
+        },
+      },
+    },
+    memoryCache,
+    distributedCache,
+  );
+});
+
+const key = "test";
+const input = Object.freeze({
+  x: new Date().toString(),
+  y: 12,
+  z: "hello",
+});
+
+describe("TieredCacheLayer read/write tests", () => {
+  it("can read and write the same value from both caches", async () => {
+    await sut.set({ key, value: input });
+
+    const output = await sut.get<typeof input>(key, false);
+
+    expect(output).toMatchObject(input);
+  });
+
+  it("will expire entry after strict ttl passed", async () => {
+    await sut.set({ key, value: input });
+
+    vitest.advanceTimersByTime(1); // To prevent off-by-one
+    vitest.advanceTimersByTime(memoryStrictTtl * 1000);
+
+    const memoryExpired = await sut.get<typeof input>(key, false);
+    const inMemory = await memoryCache.get(key);
+
+    expect(memoryExpired).toMatchObject(input);
+    expect(inMemory).toBeUndefined();
+
+    vitest.advanceTimersByTime((distributedStrictTtl - memoryStrictTtl) * 1000);
+
+    const after = await sut.get<typeof input>(key, false);
+
+    expect(after).toBeNull();
+  });
+
+  it("will soft expire entry after soft ttl passes", async () => {
+    await sut.set({ key, value: input });
+
+    vitest.advanceTimersByTime(1); // To prevent off-by-one
+    vitest.advanceTimersByTime(distributedSoftTtl * 1000);
+
+    const noExpiredAllowed = await sut.get<typeof input>(key, false);
+    const expiredAllowed = await sut.get<typeof input>(key, true);
+
+    expect(noExpiredAllowed).toBeNull();
+    expect(expiredAllowed).toMatchObject(input);
+  });
+
+  it("will fall back to memory if distributed is empty", async () => {
+    await sut.set({ key, value: input });
+    await distributedCache.clear();
+
+    const output = await sut.get<typeof input>(key, false);
+
+    expect(output).toMatchObject(input);
+  });
+
+  it("will fall back to expired memory if distributed is empty", async () => {
+    await sut.set({ key, value: input });
+    await distributedCache.clear();
+
+    const output = await sut.get<typeof input>(key, false);
+
+    expect(output).toMatchObject(input);
+
+    vitest.advanceTimersByTime(1); // To prevent off-by-one
+    vitest.advanceTimersByTime(memorySoftTtl * 1000);
+
+    const noExpiredAllowed = await sut.get<typeof input>(key, false);
+    const expiredAllowed = await sut.get<typeof input>(key, true);
+
+    expect(noExpiredAllowed).toBeNull();
+    expect(expiredAllowed).toMatchObject(input);
+  });
+});
