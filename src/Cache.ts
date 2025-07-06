df29d3c940f76f0e6518592475fb0d40aea2f103
Wessel van Lit <wessel@litdev.nl>
2025-07-06 12:57:09 +0200

    Implement getBatch without timeout/stampede protection

--- src/Cache.ts
+++ src/Cache.ts
@@ -95,51 +95,84 @@ export default class MultiTieredCache implements Cache {
     this.distributedCache = config.distributed.cache;
   }
 
-  getBatch<T extends Cacheable>(keys: Key[], factory?: (keys: Key[]) => Promise<Batch<T>>): Promise<Batch<T>> {
-    /* 
-    Query Memory -> Partition
-      Fresh
-        Return as is
-      Stale
-        Return + schedule distributed fetch
-      Miss
-        distributed fetch
-    */
+  async getBatch<T extends Cacheable>(keys: Key[], factory?: (keys: Key[]) => Promise<Batch<T>>): Promise<Batch<T>> {
+    const { fresh: freshM, stale: staleM, miss: missM } = await this.__getFromCache<T>(this.memoryCache, keys);
+
+    if (freshM.size === keys.length) {
+      return freshM;
+    }
+
+    const { freshD, staleD, missD } = await this.__getFromDistributedCache<T>(staleM, missM);
+
+    // Ensure distributed values are in the memory cache for performance & graceful fallback
+    // Not awaited to ensure it happens in the background
+    this.setBatchInCache(freshD, this.config.memory, true);
+    this.setBatchInCache(staleD, this.config.memory, false);
+
+    // Misses need to be retrieved from the factory
+    let fromFactory: Batch<T> | undefined;
+    if (factory && missD.length) {
+      fromFactory = await this.__getFromFactory<T>(staleD, missD, factory);
+
+      // Ensure new values are in both caches
+      await this.setBatch(fromFactory, true);
+    }
+
+    // Merge all fresh values into a batch
+    return new Map([...freshM, ...freshD, ...(fromFactory ? fromFactory : [])]);
+  }
+
+  async __getFromCache<T extends Cacheable>(cache: Keyv, keys: Key[]) {
+    const entries = await cache.getMany<CacheEntry<T>>(keys);
+    return this.splitByState<T>(keys, entries);
+  }
+
+  async __getFromDistributedCache<T extends Cacheable>(staleM: Batch<T>, missM: Key[]) {
+    const keys = [...missM, ...staleM.keys()];
+
     /*
-    Query Distributed
-      Acquire stampede lock per key
-        Full hit
-          Wait until done
-        Partial hit
-          Fetch keys + put in stampede lock
-          Wait until done for hit
-        Full miss
-          Fetch keys + put in stampede lock
-      
-      Partition
-        Fresh
-          Return as is
-        Stale
-          Return + schedule get from factory
-        Miss
-          Get from factory
+    TODO: Acquire stampede lock per key
+      Full hit
+        Wait until done
+      Partial hit
+        Fetch keys + put in stampede lock
+        Wait until done for hit
+      Full miss
+        Fetch keys + put in stampede lock
     */
+
+    // TODO: Handle soft timeout (return staleM as stale, missM as miss) / strict timeout (throw exception)
+    const { fresh: freshD, stale: staleD, miss: missD } = await this.__getFromCache<T>(this.distributedCache, keys);
+
+    return {
+      freshD,
+      staleD,
+      missD,
+    };
+  }
+
+  async __getFromFactory<T extends Cacheable>(
+    staleD: Batch<T>,
+    missD: Key[],
+    factory: (keys: Key[]) => Promise<Batch<T>>,
+  ) {
+    const keys = [...missD, ...staleD.keys()];
+
     /*
-    Get from factory
-      Acquire stampede lock per key
-        Full hit
-          Skip if background
-          Wait if not
-        Partial hit
-          Fetch keys + put in stampede lock
-          Skip if background / Wait until done for hit
-        Full miss
-          Fetch keys + put in stampede lock
-      Write to memory + distributed cache
+    TODO: Acquire stampede lock per key
+      Full hit
+        Skip if background
+        Wait if not
+      Partial hit
+        Fetch keys + put in stampede lock
+        Skip if background / Wait until done for hit
+      Full miss
+        Fetch keys + put in stampede lock
     */
-    /*
-    Merge returned values into a Batch
-    */
+
+    // TODO: Handle soft timeout (return staleD as stale, missD as miss) / strict timeout (throw exception)
+
+    return await factory(keys);
   }
 
   async get<T extends Cacheable>(key: Key, factory?: () => Promise<T>): Promise<T> {
@@ -221,5 +254,37 @@ export default class MultiTieredCache implements Cache {
 
   now() {
     return Date.now();
+  }
+
+  splitByState<T extends Cacheable>(
+    keys: Key[],
+    entries: (CacheEntry<T> | undefined)[],
+  ): { fresh: Batch<T>; stale: Batch<T>; miss: Key[] } {
+    const fresh: Batch<T> = new Map();
+    const stale: Batch<T> = new Map();
+    const miss: Key[] = [];
+
+    const now = this.now();
+
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const entry = entries[i];
+
+      if (entry) {
+        if (entry.freshUntil > now) {
+          fresh.set(key, entry.value);
+        } else {
+          stale.set(key, entry.value);
+        }
+      } else {
+        miss.push(key);
+      }
+    }
+
+    return {
+      fresh,
+      stale,
+      miss,
+    };
   }
 }
